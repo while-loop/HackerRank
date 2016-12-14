94ba1b8c7676bc31d330be879424b74545edad81
Anthony Alves <aalves@fit.edu>
2016-12-14 16:56:18 -0500

    Pangrams

---
+++ src/Algorithms/Strings/Pangrams/main.go
@@ -0,0 +1,41 @@
+/**
+  * Set of all letters
+  *
+  */
+
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+/**
+
+ */
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	set := map[byte]bool{}
+	for {
+		t, err := reader.ReadByte()
+		if t == 10 || err != nil {
+			break
+		}
+
+		if t != ' '{
+			if 65 <= t && t <= 90{
+				t +=32
+			}
+			set[t] = true
+		}
+	}
+
+	if len(set) == 26 {
+		fmt.Println("pangram")
+	} else {
+		fmt.Println("not pangram")
+	}
+}
