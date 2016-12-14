4113e8a990dcf9748abaa6ff2a2059376add2d74
Anthony Alves <cvballa3g0@gmail.com>
2016-12-13 21:01:33 -0500

    Fibonacci Returns

---
+++ src/contests/Programming_Interview_Questions/Fibonacci_Returns/main.go
@@ -0,0 +1,59 @@
+/**
+  * Recursive Fibonacci w/ memoization
+  *
+  */
+
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/**
+12
+144
+
+30
+832040
+ */
+
+var dp [100]*big.Int
+
+func main() {
+	initdp()
+	reader := bufio.NewReader(os.Stdin)
+
+
+	for {
+		s, _ := reader.ReadString('\n')
+		n, err := strconv.Atoi(strings.TrimSpace(s))
+		if err != nil {
+			break
+		}
+		fmt.Printf("%d\n", fibonacci(n))
+	}
+}
+
+func initdp() {
+	dp[0] = big.NewInt(0)
+	dp[1] = big.NewInt(1)
+}
+
+func fibonacci(n int) *big.Int {
+	// base cases
+	if n <= 1 || dp[n] != nil {
+		return dp[n]
+	}
+
+	// cache solution
+	dp[n - 1] = fibonacci(n - 1)
+	dp[n - 2] = fibonacci(n - 2)
+	dp[n] = &big.Int{}
+	dp[n].Add(dp[n - 1], dp[n - 2])
+	return dp[n]
+}
---
+++ src/contests/Programming_Interview_Questions/Fibonacci_Returns/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"math/big"
+)
+
+type Pair struct {
+	a int
+	b int64
+}
+
+func TestFib(t *testing.T) {
+	initdp()
+	vals := [14]Pair{{1, 1}, {8, 21}, {15, 610}, {22, 17711}, {29, 514229}, {36, 14930352}, {43, 433494437},
+		{50, 12586269025}, {57, 365435296162}, {64, 10610209857723}, {71, 308061521170129}, {78, 8944394323791464},
+		{85, 259695496911122585}, {92, 7540113804746346429}}
+	for i := 0; i < len(vals); i++ {
+		assert.Equal(t, big.NewInt(vals[i].b), fibonacci(vals[i].a), "Fibonacci not match", vals[i])
+	}
+}
