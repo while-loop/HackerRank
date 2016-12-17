97e67adcc0512247812aa3c23f928a35866f6e4a
Anthony Alves <cvballa3g0@gmail.com>
2016-12-16 20:08:27 -0500

    [HackerEarth] Modify Sequence

---
+++ hackerearth/modify_sequence.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+	"strconv"
+)
+
+/**
+  * https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/modify-sequence/
+  */
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan(); n, _ := strconv.Atoi(scanner.Text())
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		scanner.Scan(); arr[i], _ = strconv.Atoi(scanner.Text())
+	}
+
+	for i := 0; i < n - 1; i++ {
+		if arr[i] < 0 {
+			fmt.Println("NO")
+			os.Exit(0)
+		}
+
+		arr[i + 1] -= arr[i];
+		arr[i] = 0;
+	}
+
+	for i := 0; i < n; i++ {
+		if arr[i] != 0 {
+			fmt.Println("NO")
+			os.Exit(0)
+		}
+	}
+
+	fmt.Println("YES")
+}
