d7981dfdf20980e2838ed55c27aa15dad22e4b97
Anthony Alves <cvballa3g0@gmail.com>
2018-01-08 21:01:10 -0500

    matrix chain mult

---
+++ misc/matrix-chain-mult/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+var in = bufio.NewScanner(os.Stdin)
+
+// https://www.geeksforgeeks.org/dynamic-programming-set-8-matrix-chain-multiplication/
+func main() {
+	in.Split(bufio.ScanWords)
+
+	N := NextInt()
+	arr := make([]int, N)
+	for i := 0; i < N; i++ {
+		arr[i] = NextInt()
+	}
+
+	fmt.Println(MatrixChainOrder(arr))
+}
+
+func MatrixChainOrder(matrix []int) int {
+	return matrixChainOrder(matrix, 1, len(matrix)-1)
+}
+
+func matrixChainOrder(matrix []int, start, end int) int {
+	if start == end { // base case
+		return 0
+	}
+
+	min := math.MaxInt32
+	for i := start; i < end; i++ {
+		if calcs := (matrix[start-1] * matrix[i] * matrix[end]) +
+			matrixChainOrder(matrix, start, i) +
+			matrixChainOrder(matrix, i+1, end); calcs < min {
+			min = calcs
+		}
+	}
+
+	return min
+}
+
+func NextInt() int {
+	in.Scan()
+	tmp, err := strconv.Atoi(in.Text())
+	if err != nil {
+		panic(err)
+	}
+
+	return tmp
+}
---
+++ misc/matrix-chain-mult/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCases(t *testing.T) {
+	tcs := []struct {
+		arr             []int
+		multiplications int
+	}{
+		{[]int{1, 2, 3, 4, 3}, 30},
+		{[]int{10, 30, 5, 60}, 4500},
+		{[]int{40, 20, 30, 10, 30}, 26000},
+		{[]int{10, 20, 30, 40, 30}, 30000},
+		{[]int{10, 20, 30}, 6000},
+	}
+
+	for idx, tc := range tcs {
+		t.Run(fmt.Sprint(idx), func(t *testing.T) {
+			assert.Equal(t, tc.multiplications, MatrixChainOrder(tc.arr))
+		})
+	}
+}
